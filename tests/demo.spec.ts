aadd65bf100eaa84fc845d664177481e83c3a8a3
mydt <thuymyduong2708@gmail.com>
2022-12-16 23:41:28 +0700

    test

---
+++ tests/demo.spec.ts
@@ -0,0 +1,19 @@
+import {test} from "@playwright/test";
+
+test.describe('Test tool QA', () => {
+    test("Elements", async ({page}) => {
+        await page.goto("https://demoqa.com/");
+        await page.click("text=Elements");
+        await page.getByText('Text Box').click();
+        await page.getByPlaceholder('Full Name').click();
+        await page.getByPlaceholder('Full Name').fill('Duong Thi My');
+        await page.getByPlaceholder('name@example.com').click();
+        await page.getByPlaceholder('name@example.com').fill('myduong@gmail.com');
+        await page.getByPlaceholder('Current Address').click();
+        await page.getByPlaceholder('Current Address').fill('Ha Dong, Ha Noi');
+        await page.locator('#permanentAddress').click();
+        await page.locator('#permanentAddress').fill('Ha Noi');
+        await page.getByRole('button', {name: 'Submit'}).click();
+        await page.close();
+    });
+});
